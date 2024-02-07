e3e2f1d2d48b9ce3e2d31af0318f440d1e2a8e0d
Azkal182 <mohazkalarif@gmail.com>
2024-02-07 20:29:36 +0700

    change format

--- src/utils/connectRedis.ts
+++ src/utils/connectRedis.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
+import dotenv from "dotenv";
+dotenv.config();
 
-const redisUrl = `redis://localhost:6379`;
+const redisUrl = process.env.REDIS_URL || `redis://localhost:6379`;
 const redisClient = createClient({
   url: redisUrl,
 });
